67d13a97984664b454076220c844e3f9c816bfca
NemanjaStamenkovic <114349561+NemanjaStamenkovic@users.noreply.github.com>
2022-10-06 22:36:58 +0200

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,37 @@
+import os
+import art
+clear = lambda: os.system('cls')
+
+
+def calc():
+    print(art.logo)
+    n1 = int(input("What is the first number?: "))
+    n2 = int(input("What is the second Number?: "))
+    def addition(n1, n2):
+        return n1 + n2
+    def substraction(n1, n2):
+        return n1 - n2
+    def multyply(n1, n2):
+        return n1 * n2
+    def divide(n1, n2):
+        return n1 / n2
+    operations = {"+": addition, "-": substraction, "/": divide, "*": multyply}
+    for x in operations:
+        print(x)
+    operator = input("Choose an operator: ")
+
+    calculationFunction = operations[operator]
+    answer = calculationFunction(n1, n2)
+    print(f"{n1} {operator} {n2} = {answer}")
+    shouldCont = input(f"Type 'y' to continue calculating with {answer}, or type 'no' to start new calculation: ")
+    while shouldCont == "y":
+        n1 = answer
+        n2 = int(input("What is the second Number?: "))
+        calculationFunction = operations[operator]
+        answer = calculationFunction(n1, n2)
+        print(f"{n1} {operator} {n2} = {answer}")
+        shouldCont = input(f"Type 'y' to continue calculating with {answer}, or type 'no' to start new calculation: ")
+    else:
+        clear()
+        calc()
+calc()
